019d9a04234f4695eddeacb62c2bc7d7504426cd
yasuaki640 <yasuaki640@gmail.com>
2019-12-31 16:40:40 +0900

    tamesi

---
+++ test/Partition.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Partition {
+    static void swap(int[] a, int idx1, int idx2) {
+        int tmp = a[idx1];
+        a[idx1] = a[idx2];
+        a[idx2] = tmp;
+    }
+
+    static void partition(int[] a, int n) {
+        int L = 0;
+        int R = n - 1;
+        int x = a[n / 2];// 枢軸
+
+        // 配列を枢軸で分割
+        do {
+            while (a[L] < x)
+                L++;
+            while (a[R] > x)
+                R--;
+            if (L <= R)
+                swap(a, L++, R--);
+
+        } while (L <= R);
+    }
+}
