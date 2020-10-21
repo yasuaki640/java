6ee0efc8a93f407cfd3ae70f80028113325bdd55
yasuaki640 <yasuaki640@gmail.com>
2020-10-22 07:37:00 +0900

    create bin tree

--- test/BinTree.java
+++ test/BinTree.java
@@ -59,4 +59,109 @@ public class BinTree<K, V> {
                 p = p.right;
         }
     }
+
+    // nodeを根とする部分木にノードを挿入する
+    private void addNode(Node<K, V> node, K key, V data) {
+        int cond = comp(key, node.getKey());
+        if (cond == 0)
+            return;
+        else if (cond < 0) {
+            if (node.left == null)
+                node.left = new Node<K, V>(key, data, null, null);
+            else
+                addNode(node.left, key, data); // 左部分木に着目
+        } else {
+            if (node.right == null)
+                node.right = new Node<K, V>(key, data, null, null);
+            else
+                addNode(node.right, key, data); // 右部分木に着目
+        }
+    }
+
+    // ノードを挿入
+    public void add(K key, V data) {
+        if (root == null)
+            root = new Node<K, V>(key, data, null, null);
+        else
+            addNode(root, key, data);
+    }
+
+    public boolean remove(K key) {
+        Node<K, V> p = root; // 走査中のノード
+        Node<K, V> parent = null; // 走査中のノードの親ノード
+        boolean isLeftChild = true; // pはparentの左子ノードか？
+        while (true) {
+            if (p == null)// これ以上進めなければ
+                return false;
+            int cond = comp(key, p.getKey());
+            if (cond == 0)
+                break; // 探索成功
+            else {
+                parent = p;
+                if (cond < 0) {
+                    isLeftChild = true;
+                    p = p.left;
+                } else {
+                    isLeftChild = false;
+                    p = p.right;
+                }
+            }
+        }
+
+        if (p.left == null) {
+            if (p == root)
+                root = p.right;
+            else if (isLeftChild)
+                parent.left = p.right;
+            else
+                parent.right = p.right;
+        } else if (p.right == null) {
+            if (p == root)
+                root = p.left;
+            else if (isLeftChild)
+                parent.left = p.left;
+            else
+                parent.right = p.left;
+        } else {
+            parent = p;
+            Node<K, V> left = p.left;
+            isLeftChild = true;
+            while (left.right != null) { // 最大ノードleftを探索
+                parent = left;
+                left = left.right;
+                isLeftChild = false;
+            }
+            p.key = left.key;
+            p.data = left.data;
+            if (isLeftChild)
+                parent.left = left.left;
+            else
+                parent.right = left.left;
+        }
+        return true;
+    }
+
+    private void printSubTree(Node node) {
+        if (node != null) {
+            printSubTree(node.left);
+            System.out.println(node.key + " " + node.data);
+            printSubTree(node.right);
+        }
+    }
+
+    public void print() {
+        printSubTree(root);
+    }
+
+    private Node<K, V> getMinKey() {
+        if (root == null)
+            return null;
+        else {
+            Node<K, V> p = root;
+            while (p.left != null)
+                p = p.left;
+            return p;
+        }
+
+    }
 }
